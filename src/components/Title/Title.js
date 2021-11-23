9531d568cf4383f6941708f84295b7eb56b9a3ea
Sina Khodabandehloo <khodabandehloo.sina@gmail.com>
2021-11-23 11:48:27 +0330

    add: Title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Title = (props) => {
+  return <div className="Title-container">Hello from Title</div>;
+};
+
+Title.propTypes = {
+  props: PropTypes.object,
+};
